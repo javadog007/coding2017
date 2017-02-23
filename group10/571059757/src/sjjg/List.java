d00ef84ffa875e18f718ed3e5fb43d08390ba7e6
javadog007 <571059757@qq.com>
2017-02-23 20:38:54 +0800

    2月23号晚更新

---
+++ group10/571059757/src/sjjg/List.java
@@ -0,0 +1,71 @@
+package sjjg;
+/**
+ * 抽象出List标准
+ * @author mengluo
+ *
+ */
+public interface List {
+	/**
+	 * @return List集合中元素的个数
+	 */
+	 int size();
+	 
+	 /**
+	  * @return 如果List集合中没有任何元素,返回true
+	  */
+	 boolean isEmpty();
+
+	 /**
+	  * 如果被测试元素包含在List集合中,则返回true
+	  * @param o 被测试元素
+	  * @return 返回ture说明被测试元素包含在List集合中
+	  */
+	 boolean contains(Object o);
+	 
+	/**
+	 * 把List集合中的数据全都变成数组
+	 * @return
+	 */
+	 Object[] toArray();
+	 
+	 /**
+	  * 在List集合最后端添加元素
+	  * @param e
+	  * @return
+	  */
+	 boolean add(Object e);
+	 
+	 /**
+	  * 删除该元素在List集合中最小下标的数
+	  * @param o
+	  * @return
+	  */
+	 boolean remove(Object o);
+	 
+	 /**
+	  * 清空List
+	  */
+	 void clear();
+	 
+	 /**
+	  * 根据索引返回元素
+	  * @param index
+	  * @return
+	  */
+	 Object get(int index);
+	 
+	 /**
+	  * 把特定位置的元素替换掉
+	  * @param index 索引
+	  * @param element 元素
+	  * @return
+	  */
+	 Object set(int index, Object element);
+	 
+	 /**
+	  * 在特定的索引处插入元素
+	  * @param index 索引
+	  * @param element 被插入的元素
+	  */
+	 void add(int index, Object element);
+}
---
+++ group10/571059757/src/sjjg/MiniArrayList.java
@@ -0,0 +1,16 @@
+package sjjg;
+
+import java.util.ArrayList;
+
+/**
+ * 模拟实现链表结构
+ * @author mengluo
+ *
+ */
+public class MiniArrayList {
+	
+	public static void main(String[] args) {
+		ArrayList<E>
+	}
+	
+}
